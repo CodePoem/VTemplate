1fac2f5cd99fd0a5020ee02cad4e8caae6dc0f6a
CodePoem <806957428@qq.com>
2021-01-28 14:16:43 +0800

    :bug: feature: fix Jenkinsfile sh sed

--- scripts/update_file_content.sh
+++ scripts/update_file_content.sh
@@ -17,19 +17,6 @@ help_msg_update_expression(){
 
 # 更新
 start_update() {
-  if [[ `uname` == 'Darwin' ]]
-  then
-    echo "Mac OS"
-    sed -i '' $update_expression $update_file_path
-  elif [[ `uname` == 'Linux' ]]
-  then
-      echo "Linux"
-      sed -i $update_expression $update_file_path
-  fi
-}
-
-# 主函数
-main(){
   # arg judge
   #逐个接收选项及其参数
   while getopts "f:e:" arg
@@ -49,7 +36,21 @@ main(){
       ;;
     esac
   done
-  start_update
+  if [[ `uname` == 'Darwin' ]]
+  then
+    echo "Mac OS"
+    sed -i '' $update_expression $update_file_path
+  elif [[ `uname` == 'Linux' ]]
+  then
+      echo "Linux"
+      sed -i $update_expression $update_file_path
+  fi
+}
+
+# 主函数
+main(){
+  # 更新Jenkins build版本号
+  start_update -f gradle.properties -e "s#JENKINS_BUILD_NUMBER.*#JENKINS_BUILD_NUMBER=\'$BUILD_NUMBER\'#g"
 }
 
 main "$@"
